a01963de00b649cf92fdc2a7efe434795e0f281a
it20638054 <it20638054@my.sliit.lk>
2022-05-08 23:13:23 +0530

    Pet Profile Part Added

---
+++ app/src/main/java/com/example/PharmacySelect.java
@@ -0,0 +1,29 @@
+package com.example.clinipets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class PharmacySelect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pharmacy_select);
+    }
+
+    public void button2(View view)
+    {
+        Intent intent = new Intent(this, OrderMedicine.class);
+
+        Button cn = (Button) findViewById(R.id.button2);
+        String conf = cn.getText().toString();
+
+        intent.putExtra("CONFIRM", conf);
+
+        startActivity(intent);
+    }
+}
